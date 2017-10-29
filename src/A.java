c28bb91daacd9832c6a481c83ff7eee917cd79f3
Mehedy Al Hasan <mdmehedy302@gmail.com>
2017-10-30 02:10:44 +0600

    It Is not Valid

--- src/A.java
+++ src/A.java
@@ -1,1 +1,24 @@
+ class A {
+int i;
+private int j;
+void setij(int x, int y) {
+i = x;
+j = y;
+}
+}
  
+ class B extends A {
+	 int total;
+	 void sum() {
+	 total = i + j;  //Here j is private so, it is not accessable.
+	 }
+	 }
+ 
+ class Access {
+	 public static void main(String args[]) {
+	 B subOb = new B();
+	 subOb.setij(10, 12);
+	 subOb.sum();
+	 System.out.println("Total is " + subOb.total);
+	 }
+	 }
